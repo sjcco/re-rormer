eb4e32f5198f2d4fca352c04241e51a6637aa9f4
sjcco <73748496+sjcco@users.noreply.github.com>
2021-04-16 09:20:48 -0500

    Add Edit view and partial for the form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
 
     if @user.save
@@ -13,6 +13,22 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to edit_user_path
+    else
+      render :edit
+    end
+  end
+
+  private
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
